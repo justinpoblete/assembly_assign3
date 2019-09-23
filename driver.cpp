09450e7c580cc8bc4201524cf8df60e4d078afdc
justinpoblete <42792105+justinpoblete@users.noreply.github.com>
2019-09-22 23:49:41 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -3,9 +3,13 @@
 extern "C" long int assign3();
 
 int main(){
+
   long int return_code = 99;
-  printf("%s", "Welcome to statistical numbers\n Brought to you by Justin Poblete\n");
+
+  printf("%s", "Welcome to statistical numbers\nBrought to you by Justin Poblete\n");
+
   return_code = assign3();
-  printf("%s%ld%s", "This is the C++ program responding. Here we recieved the number ", return_code, ". Have a nice day.\n
-            The C++ will not return a zero to the operating system.");
+
+  printf("%s%ld%s\n","This is the C++ program responding. Here we recieved the number ",return_code,".Have a nice day.\nThe C++ program will not return a zero to the operating system.");
+
 }
