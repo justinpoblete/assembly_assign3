90257b8973557ed6369ef262b827965e8e74c486
justinpoblete <42792105+justinpoblete@users.noreply.github.com>
2019-09-22 23:49:11 -0700

    Update assign3.asm

--- assign3.asm
+++ assign3.asm
@@ -6,36 +6,35 @@
 global assign3
 extern printf
 extern scanf
+extern getchar
+
 
 segment .data
 
 ;-----Messages------
 
 
-initialmessage db "The X86 program has begun",10,0
+initialmessage db "We will compute the mean for you using only integers.",10,0
 
-promptmessage1 db "Please enter the first integer and press enter:  ", 0
+promptmessage1 db "Enter a sequence of long integers separated by white space. After the last input press Control+D. ", 0
 
-promptmessage2 db "Please enter the second integer and press enter:  ", 0
+numbers_entered db 10,"The number of numbers entered was %ld.",10,0
 
-thanks db "Thank you. ", 10 , 0
+mean db "The integer mean was %ld.", 10 , 0
 
-quotient_message1 db "The quotient of %ld", 0
-quotient_message2 db " divided by %ld", 0
-quotient_message3 db " is %ld", 0
-quotient_message4 db " with remainder equal to %ld", 10, 0
+more_sets db "Do you have more sets of integers to process (y or n)? ", 0
 
-positive_message db "The remainder is positive.", 10, 0
-negative_message db "The remainder is negative.", 10, 0
-zero_message db "Sorry: we do not handle zero here at Ubuntu dot com.", 10, 0
-zero_message2 db "Please run this program again. ", 10, 0
-zero_message3 db "Negative 1 will be returned to the caller program. Have a nice day. ", 10, 0
+come_again db "Justin hopes you liked your means. Please come again.",10, 0
 
-return_message db "The remainder will be returned to the caller program. Have a nice day.", 10, 0
+lastmessage db "This software will return the last mean to the driver program. Bye ",10, 0
 
 stringformat db "%s", 0
 
 integerformat db "%ld", 0
+
+test1 db "Testing r12: %d", 10, 0
+
+
 
 segment .bss
 ;empty
@@ -61,6 +60,7 @@ push       r14                                              ;Back up r14
 push       r15                                              ;Back up r15
 
 
+
 ;--------initial message
 
 mov qword rax, 0
@@ -68,163 +68,94 @@ mov       rdi, stringformat
 mov       rsi, initialmessage
 call      printf
 
-;--------prompt for first int
+;--------prompt for set
 
 mov qword rax, 0
 mov       rdi, stringformat
 mov       rsi, promptmessage1
 call      printf
 
-;-------obtain first int
+;-------obtain set
+
+mov        r15, 0       ;counter = 0
+mov        r13, 0
+begin_loop:
+
+push qword 0
+mov qword  rax, 0
+mov        rdi, integerformat
+mov        rsi, rsp
+call       scanf
+
+cdqe                    ;extend eax to all of rax
+
+cmp        rax, -1
+je         loop_finished
+mov      r14, [rsp]   ;grab number
+add        r13, r14     ;add to r13
+pop        rax
+inc        r15          ;increment counter
+jmp        begin_loop
+
+loop_finished:
+pop       rax
+
+
+;------display numbers entered
+
+mov qword rax, 0
+mov       rdi, numbers_entered
+mov       rsi, r15
+call      printf
+
+;------calculate mean
+
+mov      rax, r13
+mov      rbx, r15
+cqo
+idiv     rbx
+mov      r13, rax
+
+;-------display mean
+
+mov qword rax, 0
+mov       rdi, mean
+mov       rsi, r13
+call      printf
+
+;------ask again for more sets
+
+mov qword rax, 0
+mov       rdi, stringformat
+mov       rsi, more_sets
+call      printf
+
+;------get char
 
 mov       rax, 0
 mov       rdi, integerformat
 push      qword 0
 mov       rsi, rsp
-call      scanf
-pop       r15
+call      getchar
+pop       r12
 
-
-;--------prompt for second int
-
-mov qword rax, 0
-mov       rdi, stringformat
-mov       rsi, promptmessage2
-call      printf
-
-;-------obtain second int
-
-mov       rax, 0
-mov       rdi, integerformat
-push     qword 0
-mov       rsi, rsp
-call      scanf
-pop       r14
-
-;------zero_message
-
-cmp       r14, 0
-je        equalzero
-jmp       continue3
-equalzero:
-mov qword rax, 0
-mov       rdi, stringformat
-mov       rsi, zero_message
-call      printf
+;-------test1 display r12
 
 mov qword rax, 0
-mov       rdi, stringformat
-mov       rsi, zero_message2
-call      printf
-
-mov qword rax, 0
-mov       rdi, stringformat
-mov       rsi, zero_message3
-call      printf
-
-mov rax, -1
-
-
-pop        r15                                              ;Restore r15
-pop        r14                                              ;Restore r14
-pop        r13                                              ;Restore r13
-pop        r12                                              ;Restore r12
-pop        r11                                              ;Restore r11
-pop        r10                                              ;Restore r10
-pop        r9                                               ;Restore r9
-pop        r8                                               ;Restore r8
-pop        rdi                                              ;Restore rdi
-pop        rsi                                              ;Restore rsi
-pop        rdx                                              ;Restore rdx
-pop        rcx                                              ;Restore rcx
-pop        rbx                                              ;Restore rbx
-pop        rbp                                              ;Restore rbp
-
-ret
-
-continue3:
-
-;------thanks
-
-mov qword rax, 0
-mov       rdi, stringformat
-mov       rsi, thanks
-call      printf
-
-;-------divide the two ints
-
-mov       rax, r15
-mov       rbx, r14
-cqo
-idiv      rbx
-mov       r13, rax    ;answer
-mov       r12, rdx    ;remainder
-
-;-------quotient_message1
-
-mov qword rax, 0
-mov       rdi, quotient_message1
-mov       rsi, r15
-call      printf
-
-;-------quotient_message2
-
-mov qword rax, 0
-mov       rdi, quotient_message2
-mov       rsi, r14
-call      printf
-
-;-------quotient_message3
-
-mov qword rax, 0
-mov       rdi, quotient_message3
-mov       rsi, r13
-call      printf
-
-;-------quotient_message4
-
-mov qword rax, 0
-mov       rdi, quotient_message4
+mov       rdi, test1
 mov       rsi, r12
 call      printf
 
-;------positive_message
-
-cmp       r12, 0
-jg        greaterthan
-jmp       continue1
-greaterthan:
-mov qword rax, 0
-mov       rdi, stringformat
-mov       rsi, positive_message
-call      printf
-continue1:
-
-;------negative_message
-
-cmp       r12, 0
-jl        lessthan
-jmp       continue2
-lessthan:
-mov qword rax, 0
-mov       rdi, stringformat
-mov       rsi, negative_message
-call      printf
-continue2:
-
-;------return_message
+;-------last Message
 
 mov qword rax, 0
 mov       rdi, stringformat
-mov       rsi, return_message
+mov       rsi, lastmessage
 call      printf
 
+;------return last mean
 
-
-;------return 5
-
-mov rax, r12
-
+mov   rax, r13
 
 pop        r15                                              ;Restore r15
 pop        r14                                              ;Restore r14
